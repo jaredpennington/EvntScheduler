ebc374535cba0ec399669ad615a65d869a006f4d
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-07-19 00:52:36 -0400

    i can style now

---
+++ client/src/components/PartyForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const PartyForm = () => {
+    
+    return (
+        <div className="text-2xl flex justify-center">like what the fuck man</div>
+    );
+}
+
+export default PartyForm;
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/components/PartyForm/index.jsx"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

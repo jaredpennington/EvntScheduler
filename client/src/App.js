c4330f770581f47d3577fe02093d15033d6dd5f8
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-07-23 01:09:36 -0400

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,67 @@
-import './index.css';
-import React from 'react';
-import PartyForm from './components/PartyForm';
+import "./index.css";
+import React from "react";
+import PartyForm from "./components/PartyForm";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import AuthService from "./utils/auth";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="">
-     <PartyForm className=""></PartyForm>
-    </div>
+    <ApolloProvider client={client}>
+      <Routes>
+        <main>
+          <div className="">
+            <Route path="/survey/:id">
+              <PartyForm className=""></PartyForm>
+            </Route>
+            <Route
+              path="/"
+              element={AuthService.loggedIn() ? <Dashboard /> : <Home />}
+            ></Route>
+            <Route path="/event/:id">
+              <Event></Event>
+            </Route>
+            <Route path="/event">
+              <EventForm></EventForm>
+            </Route>
+            <Route path="/event/:id/:guests">
+              <Event></Event>
+            </Route>
+            <Route path="/guest/:id">
+              <Event></Event>
+            </Route>
+            <Route path="/event/:id/:passwords">
+              <Event></Event>
+            </Route>
+            <Route path="/password/:id">
+              <Event></Event>
+            </Route>
+            <Route path="*" element={<NoMatch />} />
+            
+          </div>
+        </main>
+      </Routes>
+    </ApolloProvider>
   );
 }
 

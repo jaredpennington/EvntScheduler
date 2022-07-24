4abfd3440935faca4604b5f926342ddf5ed66679
garretthilberling <ghilberling@knights.ucf.edu>
2022-07-24 01:49:25 -0400

    created a nav bar in the event page to see the passwords and guests for an event. We need a third item for the nav bar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -14,38 +14,39 @@ const Dashboard = () => {
       ) : (
         data.events.map((event, index) => (
           // put card styling in this div VVV
-          
+
           <div className="uk-child-width-expand@s" uk-grid>
-          <div key={index} className="uk-card uk-card-default uk-card-body card-padding uk-card">
-            <div>
-              <Link to={`/event/${event._id}`} class="uk-card-title">{event.event_name}</Link>
+            <div
+              key={index}
+              className="uk-card uk-card-default uk-card-body card-padding uk-card"
+            >
+              <div className="uk-card-title">{event.event_name}</div>
+              <div>
+                Guests:{" "}
+                {Object.keys(event.guests).length
+                  ? Object.keys(event.guests).length
+                  : "none"}
+              </div>
+              <div>
+                Passwords:{" "}
+                {Object.keys(event.passwords).length
+                  ? Object.keys(event.passwords).length
+                  : "none"}
+              </div>
+              <div>Considered dates for event: </div>
+              {Object.values(event.date_windows).map((date, index) => (
+                <div key={index}>
+                  {date[0]} - {date[date.length - 1]}
+                </div>
+              ))}
             </div>
-            <div>
-              <Link to={`/event/${event._id}/guests`}>Guests:</Link>{" "}
-              {Object.keys(event.guests).length
-                ? Object.keys(event.guests).length
-                : "none"}
-            </div> 
-            <div>
-              <Link to={`/event/${event._id}/passwords`}>Passwords:</Link>{" "}
-              {Object.keys(event.passwords).length
-                ? Object.keys(event.passwords).length
-                : "none"}
-            </div>
-            <div>Considered dates for event: </div>
-            {Object.values(event.date_windows).map((date, index) => (
-              <div key={index}>{date[0]} - {date[date.length - 1]}</div>
-            ))}
           </div>
-        </div>
         ))
       )}
       {!loading ? (
         // Add small card here for this button thing VVV
         <div>
-          <Link to="event/createEvent">
-            Click here to make an event!
-          </Link>
+          <Link to="event/createEvent">Click here to make an event!</Link>
         </div>
       ) : (
         <></>
--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useQuery } from "@apollo/client";
+import { QUERY_EVENT } from "../utils/queries";
+import { useParams, Link } from "react-router-dom";
 
 // single event page will have a calendar that shows the names of each person who is available on a given day.
 const Event = () => {
+  let event_id = useParams().id;
   return (
-    <>
-    </>
+    <div>
+      <nav>
+        <ul className="">
+          <li><Link to={`/event/${event_id}/guests`}>Guests</Link></li>
+          <li><Link to={`/event/${event_id}/passwords`}>Passwords</Link></li>
+          <li><Link to={`/event/${event_id}/passwords`}>Third thing lol</Link></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 

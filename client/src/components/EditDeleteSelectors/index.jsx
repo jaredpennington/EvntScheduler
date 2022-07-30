e963bca5823305d5d9bbf2ecd873388a82123e09
garretthilberling <ghilberling@knights.ucf.edu>
2022-07-30 16:25:06 -0400

    added warning for deleting events, guests, and passwords

--- client/src/components/EditDeleteSelectors/index.jsx
+++ client/src/components/EditDeleteSelectors/index.jsx
@@ -16,10 +16,12 @@ const EditDeleteSelectors = ({
         this.eventId = eventId;
         this.target = "guest";
         this.delete = () => {
-          removeGuest({
-            variables: { id: guestId, eventId: eventId },
-          });
-          window.location.href = `/event/${eventId}/guests`;
+          if(window.confirm(`Permanently delete this ${this.target}?`)) {
+            removeGuest({
+              variables: { id: guestId, eventId: eventId },
+            });
+            window.location.href = `/event/${eventId}/guests`;
+          }
         };
       }
       // delete password
@@ -28,11 +30,12 @@ const EditDeleteSelectors = ({
         this.eventId = eventId;
         this.target = "password";
         this.delete = () => {
-          console.log(eventId);
-          removePassword({
-            variables: { id: passwordId, eventId: eventId },
-          });
-          window.location.href = `/event/${eventId}/passwords`;
+          if(window.confirm(`Permanently delete this ${this.target}?`)) {
+            removePassword({
+              variables: { id: passwordId, eventId: eventId },
+            });
+            window.location.href = `/event/${eventId}/passwords`;
+          }
         };
       }
       // delete event
@@ -40,10 +43,12 @@ const EditDeleteSelectors = ({
         this.id = eventId;
         this.target = null;
         this.delete = () => {
-          removeEvent({
-            variables: { id: eventId },
-          });
-          window.location.href = "/";
+          if(window.confirm(`Permanently delete this event?`)) {
+            removeEvent({
+              variables: { id: eventId },
+            });
+            window.location.href = "/";
+          }
         };
       }
     }

21a470c5ffb3d37566382af34d173f5a2696297f
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-07-31 01:22:56 -0400

    dropped database. made first and last name nore required to be unique

--- server/models/User.js
+++ server/models/User.js
@@ -7,13 +7,11 @@ const userSchema = new Schema(
         first_name: {
             type: String,
             required: true,
-            unique: true,
             trim: true
         },
         last_name: {
             type: String,
             required: true,
-            unique: true,
             trim: true
         },
         email: {

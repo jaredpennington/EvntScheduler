ddf2973e2dc8b78f7f4d242e54518206696628b1
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-08-01 17:25:47 -0400

    hember

--- client/src/pages/PasswordUpdate.jsx
+++ client/src/pages/PasswordUpdate.jsx
@@ -3,6 +3,7 @@ import UpdatePasswordForm from "../components/UpdatePasswordForm";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import { useParams } from "react-router-dom";
+import BurgerMenu from '../components/BurgerMenu';
 
 // the survey that will be sent out to guests. Responses will be stored into Guests in the database
 const EventPassword = () => {
@@ -11,6 +12,8 @@ const EventPassword = () => {
     <div>
       <Header />
       <NavBar event_id={eventId} />
+      <BurgerMenu event_id={eventId} />
+
       <UpdatePasswordForm />
     </div>
   )
